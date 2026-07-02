15f4bc0fcc3658547aa3670bfb1d46ae3613635e
schapman14663 <schapman14663@gmail.com>
2026-07-02 09:55:31 +0100

    more work on exercises and sets

--- src/exercises.rs
+++ src/exercises.rs
@@ -2,5 +2,15 @@ struct Exercise {
     name: string,
     setup: string,
     equipment: string,
-    
+    set: struct
+}
+
+struct Set {
+    weight: f32,
+    high_target_reps: f32,
+    low_target_reps: f32,
+    actual_reps: f32,
+    progression_weight: f32,
+    notes: string,
+    rest: f32
 }
